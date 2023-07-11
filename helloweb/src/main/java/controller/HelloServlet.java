5c4df8297b5bb8da9a1f0d15aea9d05791e81623
김이화 <@DESKTOP-OAHFS92>
2023-07-11 13:59:58 +0900

    helloweb 01

--- helloweb/src/main/java/controller/HelloServlet.java
+++ helloweb/src/main/java/controller/HelloServlet.java
@@ -11,14 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();
-		pw.println("<h1>Hello world</h1>");
+		pw.println("<h1>Hello World</h1>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
