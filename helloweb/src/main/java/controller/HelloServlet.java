20baa577d1cb8902c286966703926c041c207dac
김이화 <@DESKTOP-OAHFS92>
2023-07-17 17:45:58 +0900

    helloweb

--- helloweb/src/main/java/controller/HelloServlet.java
+++ helloweb/src/main/java/controller/HelloServlet.java
@@ -13,12 +13,12 @@ public class HelloServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8");
+		
 		PrintWriter pw = response.getWriter();
-		pw.println("<h1>안녕 세상~</h1>");
+		pw.println("<h1>Hello World</h1>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
